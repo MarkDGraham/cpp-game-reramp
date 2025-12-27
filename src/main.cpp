cfcd1189a58f2530538b2ff021a4a282aa0f63fa
Mark Graham <50025074+MarkDGraham@users.noreply.github.com>
2025-12-26 16:21:56 -0800

    Add delta time and structured game loop

--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,34 @@
 #include <thread>
 #include <chrono>
 
+void ProcessInput();
+
+void Update(float);
+
+void Render(int);
+
 int main()
 {
 	bool running = true;
 	int frame = 0;
 	constexpr int FRAME_DELAY_MS = 500;
 
+	using clock = std::chrono::steady_clock;
 	
+	auto lastTime = clock::now();
 	
 	while (running)
 	{
-		std::cout << "Frame: " << frame++ << std::endl;
+		auto currentTime = clock::now();
+		std::chrono::duration<float> delta = currentTime - lastTime;
+		float deltaTime = delta.count();
+		lastTime = currentTime;
+		
+		ProcessInput();
+		Update(deltaTime);
+		Render(frame++);
+		
+		
 		std::this_thread::sleep_for(
 			std::chrono::milliseconds(FRAME_DELAY_MS)
 		);
@@ -20,3 +37,18 @@ int main()
 
 	return 0;
 }
+
+void ProcessInput(){
+
+}
+
+void Update(float deltaTime){
+	std::cout << "Delta: " << deltaTime << std::endl;
+}
+
+void Render(int frame){
+	std::cout << "Frame: " << frame << std::endl;
+}
+
+
+
